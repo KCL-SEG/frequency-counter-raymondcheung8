b3b5d444f6e185995c03fb5a58c53551b12933ca
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2022-10-03 01:27:34 +0100

    Attempt 1 for displaying frequencies

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        theItem = str(item)
+        if theItem in frequencies:
+            frequencies[theItem] += 1
+        else:
+            frequencies[theItem] = 1
     return frequencies
